e52c661b519e36280c5859045230db51072353bf
Santiago_Sitlen <santiago@sitlen.com>
2022-11-28 23:12:29 -0500

    Primer commit

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,8 +6,15 @@ import Waiter from '../models/restaurant/waiter';
 import nodemailer = require('nodemailer')
 import * as jwt from 'jsonwebtoken';
 
+const accountSid = "ACefc662245232bf3edef8448e82e8c4d3";
+const authToken = "2058932182361ee5408fc7c5736c85dd";
+const client = require('twilio')(accountSid, authToken);
+
 export const login = async (req: Request, res: Response) => {
     try {
+        client.messages
+        .create({body: 'Hi there', from: '+12057493933', to: '+573122664400'})
+        .then(message => console.log(message.body));
         const user = await User.findOne({ email: req.body.email });
         if (!user) {
             return res.status(404).json({ msg: 'Oops, usuario incorrecto' });
