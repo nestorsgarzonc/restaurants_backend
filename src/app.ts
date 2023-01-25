c680b2e987dfd62f6ada738a5c0752df8e135386
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-24 20:22:17 -0500

    Accept images

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,8 @@ socketServer(app);
 
 app.disable('x-powered-by')
 
-app.use(express.json())
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb' }));
 
 app.use(cors({
     "origin": "*",
