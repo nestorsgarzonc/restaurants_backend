54c8af33475935d2a86f832e8f9f9a9370737577
jmorenohj <jmorenoh@unal.edu.co>
2023-02-21 10:09:42 -0500

    All fix

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import tableRoutes from "./routes/table.routes";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 import orderRoutes from "./routes/order.routes";
+import chefRoutes from "./routes/chef.routes";
 import restaurantRoutes from "./routes/restaurant.routes";
 import menuRoutes from "./routes/menu.routes";
 import db from './core/db';
@@ -51,4 +52,5 @@ app.use('/api/table', tableRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/restaurant', restaurantRoutes);
-app.use('/api/menu', menuRoutes)
+app.use('/api/menu', menuRoutes);
+app.use('/api/chef',chefRoutes);
