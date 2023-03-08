bd99623201b539f451937489586add052a6b944c
jmorenohj <jmorenoh@unal.edu.co>
2023-03-07 22:05:02 -0500

    Fixed call waiter

--- src/core/constants/sockets.events.ts
+++ src/core/constants/sockets.events.ts
@@ -1,7 +1,7 @@
 export const listOfOrders = 'list_of_orders';
 export const onPayedAccount = 'on_payed_account';
 export const newUserJoined = 'new_user_joined';
-export const customerRequests = 'costumers_requests';
+export const customerRequests = 'customers_requests';
 export const restaurantTables = 'restaurant:tables';
 export const orderQueue = 'order_queue';
 export const error = 'error';
